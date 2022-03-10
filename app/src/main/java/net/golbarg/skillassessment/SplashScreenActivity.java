65581d095549ade929edda1e9d5e0a91e42963fa
MINE <mustafapoyaabc@gmail.com>
2022-03-11 01:54:58 +0430

    add json insertion to db in splash activity

--- app/src/main/java/net/golbarg/skillassessment/SplashScreenActivity.java
+++ app/src/main/java/net/golbarg/skillassessment/SplashScreenActivity.java
@@ -1,15 +1,28 @@
 package net.golbarg.skillassessment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import net.golbarg.skillassessment.controller.JsonController;
+import net.golbarg.skillassessment.db.TableCategory;
+import net.golbarg.skillassessment.db.TableQuestion;
+import net.golbarg.skillassessment.util.UtilController;
+
 public class SplashScreenActivity extends AppCompatActivity {
     public static final String TAG = SplashScreenActivity.class.getName();
+    SharedPreferences pref = UtilController.getSharedPref(getApplicationContext(), "db_content");
+
     boolean isActive = true;
+
+    ProgressBar progressLoading;
+    TextView txtStatus;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,9 +33,27 @@ public class SplashScreenActivity extends AppCompatActivity {
         Thread splashThread = new Thread(){
             @Override
             public void run() {
+                try {
+                    String dbStatus = pref.getString(UtilController.KEY_DB_STATUS, null);
+
+                    if("success".equals(dbStatus)) {
+                        showWaiting();
+                    } else {
+                        addDataToDatabase();
+                    }
+
+                    finish();
+                    startActivity(new Intent(getBaseContext(), MainActivity.class));
+                } catch (Exception e) {
+                    finish();
+                    e.printStackTrace();
+                }
+            }
+
+            private void showWaiting() {
                 int waitTime = 0;
 
-                while(waitTime <= 3 /*&& isActive*/) {
+                while(waitTime <= 2) {
                     try{
                         sleep(1000);
                     }catch (InterruptedException e) {
@@ -30,9 +61,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                     }
                     waitTime += 1;
                 }
-                isActive = false;
-                finish();
-                startActivity(new Intent(getBaseContext(), MainActivity.class));
             }
         };
         splashThread.start();
@@ -44,5 +72,23 @@ public class SplashScreenActivity extends AppCompatActivity {
             isActive = false;
         }
         return true;
+    }
+
+    private void addDataToDatabase() {
+        String categoryAdded = pref.getString(TableCategory.TABLE_NAME, null);
+        if(!"added".equals(categoryAdded)) {
+            JsonController.insertCategoriesToDB(getApplicationContext());
+            UtilController.insertSharedPref(pref, TableCategory.TABLE_NAME, "added");
+        }
+
+        String questionAdded = pref.getString(TableQuestion.TABLE_NAME, null);
+        if(!"added".equals(questionAdded)) {
+            JsonController.insertQuestionsToDB(getApplicationContext());
+            UtilController.insertSharedPref(pref, TableQuestion.TABLE_NAME, "added");
+        }
+
+        // After Successful All Data Insertion
+        UtilController.insertSharedPref(pref, UtilController.KEY_DB_STATUS, "success");
+
     }
 }
