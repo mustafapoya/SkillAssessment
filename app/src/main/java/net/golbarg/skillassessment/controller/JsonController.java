c754b118964252cad57f9f2df9fb00eb1c9eb44f
MINE <mustafapoyaabc@gmail.com>
2022-03-11 01:54:01 +0430

    Create JsonController

---
+++ app/src/main/java/net/golbarg/skillassessment/controller/JsonController.java
@@ -0,0 +1,46 @@
+package net.golbarg.skillassessment.controller;
+
+import android.content.Context;
+
+import net.golbarg.skillassessment.db.DatabaseHandler;
+import net.golbarg.skillassessment.db.TableCategory;
+import net.golbarg.skillassessment.db.TableQuestion;
+import net.golbarg.skillassessment.models.Category;
+import net.golbarg.skillassessment.models.Question;
+import net.golbarg.skillassessment.util.JsonUtil;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class JsonController {
+
+    public static boolean insertCategoriesToDB(Context context) {
+        DatabaseHandler handler = new DatabaseHandler(context);
+        TableCategory tableCategory = new TableCategory(handler);
+        JSONArray jsonArrayCategory = JsonUtil.getJsonCategories(context);
+        ArrayList<Category> categoryArrayList = JsonUtil.mapCategoriesFromJson(jsonArrayCategory);
+        tableCategory.emptyTable();
+
+        for (int i = 0; i < categoryArrayList.size(); i++) {
+            tableCategory.create(categoryArrayList.get(i));
+        }
+
+        return true;
+    }
+
+    public static boolean insertQuestionsToDB(Context context) {
+        DatabaseHandler handler = new DatabaseHandler(context);
+        TableQuestion tableQuestion = new TableQuestion(handler);
+        JSONArray jsonArrayQuestion = JsonUtil.getJSONQuestion(context);
+        ArrayList<Question> questionArrayList = JsonUtil.mapQuestionsFromJson(jsonArrayQuestion);
+        tableQuestion.emptyTable();
+
+        for (int i = 0; i < questionArrayList.size(); i++) {
+            tableQuestion.create(questionArrayList.get(i));
+        }
+
+        return true;
+
+    }
+}
