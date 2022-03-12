84d05ba6fdcf8ddb6a0ea71d4ff0d0ac4adf3b4d
MINE <mustafapoyaabc@gmail.com>
2022-03-12 09:06:15 +0430

    Add Category to fragment home and load category data

--- app/src/main/java/net/golbarg/skillassessment/ui/home/HomeFragment.java
+++ app/src/main/java/net/golbarg/skillassessment/ui/home/HomeFragment.java
@@ -1,46 +1,94 @@
 package net.golbarg.skillassessment.ui.home;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import net.golbarg.skillassessment.R;
 import net.golbarg.skillassessment.databinding.FragmentHomeBinding;
+import net.golbarg.skillassessment.db.DatabaseHandler;
+import net.golbarg.skillassessment.db.TableCategory;
+import net.golbarg.skillassessment.models.Category;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    public static final String TAG = HomeFragment.class.getName();
 
-    private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    Context context;
+    ProgressBar progressLoading;
+    private ListView listViewCategory;
+    CategoryListAdapter categoryListAdapter;
+    ArrayList<Category> categoryArrayList = new ArrayList<>();
+    private ConstraintLayout layoutMainLayout;
+    DatabaseHandler dbHandler;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        context = root.getContext();
+        dbHandler = new DatabaseHandler(context);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        progressLoading = root.findViewById(R.id.progress_loading);
+        listViewCategory = root.findViewById(R.id.list_view_category);
+        layoutMainLayout = root.findViewById(R.id.layout_main_layout);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        categoryListAdapter = new CategoryListAdapter(getActivity(), categoryArrayList, getParentFragmentManager());
+        listViewCategory.setAdapter(categoryListAdapter);
+
+        try {
+            new FetchCategoryDataTask().execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private class FetchCategoryDataTask extends AsyncTask<String, String, ArrayList<Category>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressLoading.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected ArrayList<Category> doInBackground(String... params) {
+            ArrayList<Category> result = new ArrayList<>();
+
+            try {
+
+                TableCategory tableCategory = new TableCategory(dbHandler);
+                result = tableCategory.getAll();
+
+                return result;
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Category> result) {
+            super.onPostExecute(result);
+            progressLoading.setVisibility(View.GONE);
+            categoryArrayList.clear();
+            categoryArrayList.addAll(result);
+            categoryListAdapter.notifyDataSetChanged();
+        }
     }
+
 }
