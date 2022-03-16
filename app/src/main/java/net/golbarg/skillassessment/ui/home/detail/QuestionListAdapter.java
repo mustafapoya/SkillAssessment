24fc7bbd719582ccfddfa67c073cd24491861be9
MINE <mustafapoyaabc@gmail.com>
2022-03-16 21:14:51 +0430

    update QuestionListAdapter add code highlighter

--- app/src/main/java/net/golbarg/skillassessment/ui/home/detail/QuestionListAdapter.java
+++ app/src/main/java/net/golbarg/skillassessment/ui/home/detail/QuestionListAdapter.java
@@ -1,6 +1,8 @@
 package net.golbarg.skillassessment.ui.home.detail;
 
 import android.app.Activity;
+import android.text.SpannableString;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,8 @@ import com.nevidelia.library.highlight.Highlight;
 import net.golbarg.skillassessment.R;
 import net.golbarg.skillassessment.db.DatabaseHandler;
 import net.golbarg.skillassessment.models.Question;
+import net.golbarg.skillassessment.models.QuestionPart;
+import net.golbarg.skillassessment.util.UtilController;
 
 import java.util.ArrayList;
 
@@ -50,15 +54,23 @@ public class QuestionListAdapter extends ArrayAdapter<Question> {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView = inflater.inflate(R.layout.custom_list_question, null,true);
 
-        TextView txtNumber = rowView.findViewById(R.id.txt_number);
-        txtNumber.setText(String.valueOf(questions.get(position).getId()));
+//        TextView txtNumber = rowView.findViewById(R.id.txt_number);
+//        txtNumber.setText(String.valueOf(questions.get(position).getId()));
 
         TextView txtTitle = rowView.findViewById(R.id.txt_title);
         txtTitle.setText(questions.get(position).getTitle());
 
         Highlight highlight = new Highlight();
-        txtTitle.setText(highlight.java(txtTitle.getText().toString()));
-//        txtCategoryTitle.setText(highlight.code(Highlight.C,"#include<stdio.h>\nvoid main()\n{\n\b\b\b\bprintf(\"Hello\bworld\");\n}"));
+//        txtTitle.setText(highlight.java(txtTitle.getText().toString()));
+
+        QuestionPart part = UtilController.convertQuestionTextToQuestionObject(questions.get(position));
+        CharSequence questionText = TextUtils.concat(part.getTitle());
+        for(int i = 0; i < part.getCodeList().size(); i++) {
+            SpannableString result = highlight.code(part.getCodeList().get(i).getCode(), Highlight.C);
+            questionText = TextUtils.concat(questionText, result);
+        }
+
+        txtTitle.setText(questionText);
 
         rowView.setOnClickListener(new View.OnClickListener() {
             @Override
