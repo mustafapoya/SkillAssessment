9db509450eb1c4e69614a4b9ff9044ff0dfe2266
MINE <mustafapoyaabc@gmail.com>
2022-03-16 15:30:57 +0430

    create convertQuestionTextToQuestionObject method

--- app/src/main/java/net/golbarg/skillassessment/util/UtilController.java
+++ app/src/main/java/net/golbarg/skillassessment/util/UtilController.java
@@ -3,6 +3,10 @@ package net.golbarg.skillassessment.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import net.golbarg.skillassessment.models.Question;
+import net.golbarg.skillassessment.models.QuestionCode;
+import net.golbarg.skillassessment.models.QuestionPart;
+
 public class UtilController {
     public static final String KEY_HEALTH = "KEY_HEALTH";
     public static final int DEFAULT_HEALTH = 5;
@@ -41,4 +45,41 @@ public class UtilController {
         return editor.commit();
     }
 
+    public static QuestionPart convertQuestionTextToQuestionObject(Question question) {
+        QuestionPart part = new QuestionPart();
+        String questionText = question.getTitle();
+
+        if (questionText.indexOf("```") > -1) {
+            int firstIndex = 0;
+            int lastIndex = 0;
+
+            do {
+                firstIndex = questionText.indexOf("```", firstIndex);
+                lastIndex = questionText.indexOf("```", firstIndex + 2);
+
+                if (firstIndex > -1 && lastIndex > -1) {
+
+                    String codeSection = questionText.substring(firstIndex, lastIndex + 3);
+                    String programLang = codeSection.substring(3,
+                            codeSection.indexOf('\n') != -1 ? codeSection.indexOf('\n') : 3);
+                    String code = programLang.length() > 0
+                            ? codeSection.substring(3 + programLang.length(), codeSection.length() - 3)
+                            : codeSection.substring(3, codeSection.length() - 3);
+
+
+                    firstIndex = lastIndex + 3;
+
+                    questionText = questionText.replace(codeSection, "");
+
+                    part.getCodeList().add(new QuestionCode(programLang, code));
+                }
+            } while (firstIndex > -1 && lastIndex > -1);
+
+            part.setTitle(questionText);
+        } else {
+            part = new QuestionPart(questionText);
+        }
+
+        return part;
+    }
 }
