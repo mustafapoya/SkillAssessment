30ce0b94679886200691d2da2fe6ceb2f79bf287
MINE <mustafapoyaabc@gmail.com>
2022-03-16 22:39:10 +0430

    update question style

--- app/src/main/java/net/golbarg/skillassessment/ui/home/detail/QuestionListAdapter.java
+++ app/src/main/java/net/golbarg/skillassessment/ui/home/detail/QuestionListAdapter.java
@@ -61,12 +61,12 @@ public class QuestionListAdapter extends ArrayAdapter<Question> {
         txtTitle.setText(questions.get(position).getTitle());
 
         Highlight highlight = new Highlight();
-//        txtTitle.setText(highlight.java(txtTitle.getText().toString()));
+//        highlight.setColors("#FE9003", "#FF0000", "#858585", "#cccccc", "#6A9955", "#b180d7", "#000000");
 
         QuestionPart part = UtilController.convertQuestionTextToQuestionObject(questions.get(position));
         CharSequence questionText = TextUtils.concat(part.getTitle());
         for(int i = 0; i < part.getCodeList().size(); i++) {
-            SpannableString result = highlight.c(part.getCodeList().get(i).getCode());
+            SpannableString result = highlight.c(part.getCodeList().get(i).getCode().trim().replace("    ", "  "));
             questionText = TextUtils.concat(questionText, result);
         }
 
