f1a9cb10678b0c7bc035d3aa20953cdfb7348be5
MINE <mustafapoyaabc@gmail.com>
2022-04-02 04:33:35 +0430

    add pie chart graph

--- app/src/main/java/net/golbarg/skillassessment/ui/question/QuestionResultActivity.java
+++ app/src/main/java/net/golbarg/skillassessment/ui/question/QuestionResultActivity.java
@@ -2,18 +2,31 @@ package net.golbarg.skillassessment.ui.question;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 import net.golbarg.skillassessment.R;
 import net.golbarg.skillassessment.db.DatabaseHandler;
 import net.golbarg.skillassessment.db.TableCategory;
 import net.golbarg.skillassessment.db.TableQuestionResult;
 import net.golbarg.skillassessment.models.Category;
+import net.golbarg.skillassessment.models.PieChartData;
 import net.golbarg.skillassessment.models.QuestionResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionResultActivity extends AppCompatActivity {
     public static final String TAG = QuestionResultActivity.class.getName();
@@ -32,6 +45,7 @@ public class QuestionResultActivity extends AppCompatActivity {
     TextView txtQuestionWrong;
     TextView txtQuestionNoAnswer;
     TextView txtProgressStatus;
+    PieChart pieChartProgress;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,12 +63,14 @@ public class QuestionResultActivity extends AppCompatActivity {
         txtQuestionWrong = findViewById(R.id.txt_question_wrong);
         txtQuestionNoAnswer = findViewById(R.id.txt_question_no_answer);
         txtProgressStatus = findViewById(R.id.txt_progress_status);
+        pieChartProgress = findViewById(R.id.pie_chart_progress);
 
         Intent intent = getIntent();
-        int exam_result_id = intent.getIntExtra("exam_result_id", 1);
+        long question_result_id = intent.getLongExtra("question_result_id", 1);
+        Log.d(TAG, "question_result_id: " + question_result_id);
 
-        if(exam_result_id != -1) {
-            selectedQuestionResult = tableQuestionResult.get(exam_result_id);
+        if(question_result_id != -1) {
+            selectedQuestionResult = tableQuestionResult.get((int)question_result_id);
             selectedCategory = tableCategory.get(selectedQuestionResult.getCategoryId());
 
             txtCategoryTitle.setText(selectedCategory.getTitle().replace("-", " ").toUpperCase());
@@ -65,6 +81,38 @@ public class QuestionResultActivity extends AppCompatActivity {
 
             int successPercent = (selectedQuestionResult.getCorrectAnswer() * 100) / selectedCategory.getNumberOfQuestion();
             txtProgressStatus.setText("Success Rate: " + String.valueOf(successPercent) + "%");
+
+            ArrayList<PieChartData> pieChartData = new ArrayList<>();
+            pieChartData.add(new PieChartData("Correct", selectedQuestionResult.getCorrectAnswer(), context.getResources().getColor(R.color.green_500)));
+            pieChartData.add(new PieChartData("Wrong", selectedQuestionResult.getWrongAnswer(), context.getResources().getColor(R.color.red_500)));
+            pieChartData.add(new PieChartData("No Response", selectedQuestionResult.getNoAnswer(), context.getResources().getColor(R.color.gray)));
+
+
+            List<PieEntry> entries = new ArrayList<>();
+            for (PieChartData data: pieChartData) {
+                PieEntry pie = new PieEntry(data.getValue(), data.getLabel());
+                entries.add(pie);
+            }
+
+            PieDataSet dataSet = new PieDataSet(entries, "");
+            dataSet.setValueTextSize(12);
+            dataSet.setValueTextColor(context.getResources().getColor(R.color.white));
+            dataSet.setColor(context.getResources().getColor(R.color.black));
+
+            dataSet.resetColors();
+            for (PieChartData data: pieChartData) {
+                dataSet.addColor(data.getColor());
+            }
+            PieData pieData = new PieData(dataSet);
+            pieChartProgress.setData(pieData);
+            Description d = new Description();
+            d.setText("");
+            pieChartProgress.setDescription(d);
+            pieChartProgress.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            pieChartProgress.setCenterTextColor(context.getResources().getColor(R.color.blue_700));
+            pieChartProgress.setCenterText("Result");
+            pieChartProgress.setCenterTextSize(18);
+            pieChartProgress.invalidate();
 
         }
 
